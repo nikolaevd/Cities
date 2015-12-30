de98f5e44c9ac4cd672898159ff34f80bb216e41
Denis Nikolaev <idenisnikolaev@itsph.ru>
2015-12-30 21:41:36 +0300

    update create-citizens.sql

--- install/db/create-citizens.sql
+++ install/db/create-citizens.sql
@@ -2,68 +2,57 @@
 -- Table: cities
 ----------------------------------------------------
 
-CREATE SEQUENCE auto_city_id;
-ALTER SEQUENCE auto_city_id OWNER TO postgres;
-
 CREATE TABLE cities(
-	id integer NOT NULL DEFAULT nextval(auto_city_id),
+	id integer NOT NULL,
 	name varchar(32) NOT NULL,
 	year integer,
 	area integer,
 	CONSTRAINT key_cities PRIMARY KEY (id)
 )
-
 WITH ( OIDS=FALSE );
 ALTER TABLE cities OWNER TO postgres;
 
-INSERT INTO cities (name, year, area) VALUES ('Москва', 1147, 2561);
-INSERT INTO cities (name, year, area) VALUES ('Санкт-Петербург', 1703, 1439);
-INSERT INTO cities (name, year, area) VALUES ('Екатеринбург', 1723, 487);
-INSERT INTO cities (name, year, area) VALUES ('Самара', 1586, 541);
+INSERT INTO cities (id, name, year, area) VALUES (0, 'Москва', 1147, 2561);
+INSERT INTO cities (id, name, year, area) VALUES (1, 'Санкт-Петербург', 1703, 1439);
+INSERT INTO cities (id, name, year, area) VALUES (2, 'Екатеринбург', 1723, 487);
+INSERT INTO cities (id, name, year, area) VALUES (3, 'Самара', 1586, 541);
 
 ----------------------------------------------------
 -- Table: nationality
 ----------------------------------------------------
 
-CREATE SEQUENCE auto_nationality_id;
-ALTER SEQUENCE auto_nationality_id OWNER TO postgres;
-
 CREATE TABLE nationality(
-	id integer NOT NULL DEFAULT nextval(auto_nationality_id),
+	id integer NOT NULL,
 	nationality varchar(32),
 	incipience varchar(32),
 	CONSTRAINT key_nationality PRIMARY KEY (id)
 )
-
 WITH ( OIDS=FALSE );
 ALTER TABLE nationality OWNER TO postgres;
 
-INSERT INTO nationality (nationality, incipience) VALUES ('русские', 'IX век');
-INSERT INTO nationality (nationality, incipience) VALUES ('татары', 'VI век');
-INSERT INTO nationality (nationality, incipience) VALUES ('украинцы', 'IX век');
-INSERT INTO nationality (nationality, incipience) VALUES ('армяне', 'XIII век');
-INSERT INTO nationality (nationality, incipience) VALUES ('молдаване', 'XIV век');
+INSERT INTO nationality (id, nationality, incipience) VALUES (0, 'русские', 'IX век');
+INSERT INTO nationality (id, nationality, incipience) VALUES (1, 'татары', 'VI век');
+INSERT INTO nationality (id, nationality, incipience) VALUES (2, 'украинцы', 'IX век');
+INSERT INTO nationality (id, nationality, incipience) VALUES (3, 'армяне', 'XIII век');
+INSERT INTO nationality (id, nationality, incipience) VALUES (4, 'молдаване', 'XIV век');
 
 ----------------------------------------------------
 -- Table: languages
 ----------------------------------------------------
 
-CREATE SEQUENCE auto_language_id;
-ALTER SEQUENCE auto_language_id OWNER TO postgres;
-
 CREATE TABLE languages(
-	id integer NOT NULL DEFAULT nextval(auto_language_id),
-	language varchar(32)
+	id integer NOT NULL,
+	language varchar(32),
+	CONSTRAINT key_languages PRIMARY KEY (id)
 )
-
 WITH ( OIDS=FALSE );
 ALTER TABLE languages OWNER TO postgres;
 
-INSERT INTO languages (language) VALUES ('русский');
-INSERT INTO languages (language) VALUES ('татарский');
-INSERT INTO languages (language) VALUES ('украинский');
-INSERT INTO languages (language) VALUES ('армянский');
-INSERT INTO languages (language) VALUES ('молдавский');
+INSERT INTO languages (id, language) VALUES (0, 'русский');
+INSERT INTO languages (id, language) VALUES (1, 'татарский');
+INSERT INTO languages (id, language) VALUES (2, 'украинский');
+INSERT INTO languages (id, language) VALUES (3, 'армянский');
+INSERT INTO languages (id, language) VALUES (4, 'молдавский');
 
 ----------------------------------------------------
 -- Table: national_languages
@@ -75,22 +64,21 @@ CREATE TABLE national_languages(
 	CONSTRAINT key_nationality_id FOREIGN KEY (nationality_id)
 		REFERENCES nationality (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
 	CONSTRAINT key_language_id FOREIGN KEY (language_id)
-		REFERENCES language (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
+		REFERENCES languages (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
 )
-
 WITH ( OIDS=FALSE );
 ALTER TABLE national_languages OWNER TO postgres;
 
 -- ID берем из последовательности!!!
-INSERT INTO national_languages (nationality_id, lang_id) VALUES (0, 0);
-INSERT INTO national_languages (nationality_id, lang_id) VALUES (1, 0);
-INSERT INTO national_languages (nationality_id, lang_id) VALUES (1, 1);
-INSERT INTO national_languages (nationality_id, lang_id) VALUES (2, 0);
-INSERT INTO national_languages (nationality_id, lang_id) VALUES (2, 2);
-INSERT INTO national_languages (nationality_id, lang_id) VALUES (3, 0);
-INSERT INTO national_languages (nationality_id, lang_id) VALUES (3, 3);
-INSERT INTO national_languages (nationality_id, lang_id) VALUES (4, 0);
-INSERT INTO national_languages (nationality_id, lang_id) VALUES (4, 4);
+INSERT INTO national_languages (nationality_id, language_id) VALUES (0, 0);
+INSERT INTO national_languages (nationality_id, language_id) VALUES (1, 0);
+INSERT INTO national_languages (nationality_id, language_id) VALUES (1, 1);
+INSERT INTO national_languages (nationality_id, language_id) VALUES (2, 0);
+INSERT INTO national_languages (nationality_id, language_id) VALUES (2, 2);
+INSERT INTO national_languages (nationality_id, language_id) VALUES (3, 0);
+INSERT INTO national_languages (nationality_id, language_id) VALUES (3, 3);
+INSERT INTO national_languages (nationality_id, language_id) VALUES (4, 0);
+INSERT INTO national_languages (nationality_id, language_id) VALUES (4, 4);
 
 ----------------------------------------------------
 -- Table: population
@@ -103,11 +91,10 @@ CREATE TABLE population(
 	CONSTRAINT key_nationality_id FOREIGN KEY (nationality_id)
 		REFERENCES nationality (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
 	CONSTRAINT key_city_id FOREIGN KEY (city_id)
-		REFERENCES cities (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
+		REFERENCES cities (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
 )
-
 WITH ( OIDS=FALSE );
-ALTER TABLE operations OWNER TO postgres;
+ALTER TABLE population OWNER TO postgres;
 
 -- ID берем из последовательности!!!
 INSERT INTO population (nationality_id, city_id, population) VALUES (0, 0, 9930410);
