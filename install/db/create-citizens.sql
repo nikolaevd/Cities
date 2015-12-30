d79524ce11ad02726029759c1b141cde6dbd572a
Denis Nikolaev <idenisnikolaev@itsph.ru>
2015-12-30 16:15:45 +0300

    update create-citizens.sql

--- install/db/create-citizens.sql
+++ install/db/create-citizens.sql
@@ -1,7 +1,49 @@
-cities
-id | name | year | area | population
+CREATE SEQUENCE auto_city_id;
+ALTER SEQUENCE auto_city_id OWNER TO postgres;
 
+CREATE TABLE cities(
+	id integer NOT NULL DEFAULT nextval(auto_city_id),
+	name varchar(32) NOT NULL,
+	year integer,
+	area integer,
+	CONSTRAINT key_cities PRIMARY KEY (id)
+)
 
-type_of_citizens
-id | type | city_id | language
+WITH ( OIDS=FALSE );
+ALTER TABLE cities OWNER TO postgres;
 
+INSERT INTO cities (name, year, area) VALUES ('Москва', 1147, 2561);
+INSERT INTO cities (name, year, area) VALUES ('Санкт-Петербург', 1703, 1439);
+INSERT INTO cities (name, year, area) VALUES ('Екатеринбург', 1723, 487);
+INSERT INTO cities (name, year, area) VALUES ('Самара', 1586, 541);
+
+CREATE SEQUENCE auto_nationality_id;
+ALTER SEQUENCE auto_nationality_id OWNER TO postgres;
+
+CREATE TABLE nationality(
+	id integer NOT NULL DEFAULT nextval(auto_nationality_id),
+	nationality varchar(32),
+	language varchar(32),
+	incipience varchar(32)
+	CONSTRAINT key_nationality PRIMARY KEY (id)
+)
+
+WITH ( OIDS=FALSE );
+ALTER TABLE nationality OWNER TO postgres;
+
+INSERT INTO nationality (nationality, language, incipience) VALUES ('русские', 'русский', 'IX век');
+INSERT INTO nationality (nationality, language, incipience) VALUES ('татары', 'татарский', 'VI век');
+INSERT INTO nationality (nationality, language, incipience) VALUES ('украинцы', 'украинский', 'IX век');
+INSERT INTO nationality (nationality, language, incipience) VALUES ('армяне', 'армянский', 'XIII век');
+INSERT INTO nationality (nationality, language, incipience) VALUES ('якуты', 'якутский', 'XI век');
+
+CREATE TABLE population(
+	population integer,
+	city_id integer REFERENCES cities,
+	nationality_id integer REFERENCES nationality
+)
+
+WITH ( OIDS=FALSE );
+ALTER TABLE operations OWNER TO postgres;
+
+INSERT INTO population (population, language, incipience) VALUES ('русские', 0, 0);
