58578d448465a9cb6910d87b05699f9af9d07b96
Denis Nikolaev <idenisnikolaev@itsph.ru>
2016-01-01 19:11:29 +0300

    update selects-citizens.sql

--- install/db/selects-citizens.sql
+++ install/db/selects-citizens.sql
@@ -8,7 +8,7 @@ FROM nationality n
 	JOIN cities c ON c.id = p.city_id
 	JOIN national_languages nl ON n.id = nl.nationality_id
 	JOIN languages l ON l.id = nl.language_id
-	WHERE c.city = 'Москва' AND l.language = 'русский'
+WHERE c.city = 'Москва' AND l.language = 'русский'
 	
 --------------------------------------------------------------------------------------------
 -- вывести информацию обо всех городах, в которых проживают жители выбранной национальности
@@ -18,12 +18,22 @@ SELECT c.city, c.year, c.area, n.nationality
 FROM cities c
 	JOIN population p ON c.id = p.city_id
 	JOIN nationality n ON n.id = p.nationality_id
-	WHERE n.nationality = 'украинцы'
+WHERE n.nationality = 'украинцы'
 	
 --------------------------------------------------------------------------------------------
 -- вывести информацию о городе с заданным количеством населения и всех типах жителей
 --------------------------------------------------------------------------------------------
 
+SELECT c.city, c.year, c.area, n.nationality, p.population
+FROM cities c
+	JOIN population p ON c.id = p.city_id
+	JOIN nationality n ON n.id = p.nationality_id
+WHERE p.population > 1000000
+
 --------------------------------------------------------------------------------------------
 -- вывести информацию о самой древней народности (национальности)
 --------------------------------------------------------------------------------------------
+
+SELECT n.nationality, n.incipience
+FROM nationality n
+WHERE n.incipience = (SELECT MIN(n.incipience) FROM nationality n)
