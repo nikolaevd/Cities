bb9103ba7779bb01614b0f6837474b93c518b136
Denis Nikolaev <idenisnikolaev@itsph.ru>
2016-01-01 18:14:12 +0300

    current snapshot

--- install/db/create-citizens.sql
+++ install/db/create-citizens.sql
@@ -69,7 +69,6 @@ CREATE TABLE national_languages(
 WITH ( OIDS=FALSE );
 ALTER TABLE national_languages OWNER TO postgres;
 
--- ID берем из последовательности!!!
 INSERT INTO national_languages (nationality_id, language_id) VALUES (0, 0);
 INSERT INTO national_languages (nationality_id, language_id) VALUES (1, 0);
 INSERT INTO national_languages (nationality_id, language_id) VALUES (1, 1);
@@ -96,9 +95,23 @@ CREATE TABLE population(
 WITH ( OIDS=FALSE );
 ALTER TABLE population OWNER TO postgres;
 
--- ID берем из последовательности!!!
 INSERT INTO population (nationality_id, city_id, population) VALUES (0, 0, 9930410);
 INSERT INTO population (nationality_id, city_id, population) VALUES (1, 0, 149043);
 INSERT INTO population (nationality_id, city_id, population) VALUES (2, 0, 154104);
 INSERT INTO population (nationality_id, city_id, population) VALUES (3, 0, 106466);
 INSERT INTO population (nationality_id, city_id, population) VALUES (4, 0, 21699);
+INSERT INTO population (nationality_id, city_id, population) VALUES (0, 1, 3908753);
+INSERT INTO population (nationality_id, city_id, population) VALUES (1, 1, 30857);
+INSERT INTO population (nationality_id, city_id, population) VALUES (2, 1, 64446);
+INSERT INTO population (nationality_id, city_id, population) VALUES (3, 1, 19971);
+INSERT INTO population (nationality_id, city_id, population) VALUES (4, 1, 7200);
+INSERT INTO population (nationality_id, city_id, population) VALUES (0, 1, 1106688);
+INSERT INTO population (nationality_id, city_id, population) VALUES (1, 1, 46232);
+INSERT INTO population (nationality_id, city_id, population) VALUES (2, 1, 12815);
+INSERT INTO population (nationality_id, city_id, population) VALUES (3, 1, 5271);
+INSERT INTO population (nationality_id, city_id, population) VALUES (4, 1, 2013);
+INSERT INTO population (nationality_id, city_id, population) VALUES (0, 1, 986915);
+INSERT INTO population (nationality_id, city_id, population) VALUES (1, 1, 39409);
+INSERT INTO population (nationality_id, city_id, population) VALUES (2, 1, 12586);
+INSERT INTO population (nationality_id, city_id, population) VALUES (3, 1, 7424);
+INSERT INTO population (nationality_id, city_id, population) VALUES (4, 1, 1342);
