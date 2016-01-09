32c8dc6a6b248243a80855ac523a92170cd1f22f
Denis Nikolaev <idenisnikolaev@itsph.ru>
2016-01-09 18:13:09 +0300

    refactoring

--- src/java/com/example/model/CitiesDB.java
+++ src/java/com/example/model/CitiesDB.java
@@ -3,25 +3,18 @@ package com.example.model;
 import java.sql.*;
 
 public class CitiesDB {
+    
     static final String DB_URL = "jdbc:postgresql://localhost:5432/cities";
     static final String USER = "postgres";
     static final String PASS = "Iqex1234";
     
-    public static Connection getConnection(){
+    public static Connection getConnection() throws SQLException, Exception {
         
-        try{
-            String driverName = "org.postgresql.Driver";
-            Class.forName(driverName);  
-            Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
-            return connection;
-        }
-        catch(SQLException e){
-            e.getMessage();
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
-        return null; 
+        String driverName = "org.postgresql.Driver";
+        Class.forName(driverName);  
+        Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+        return connection;
+       
     }
     
 }
