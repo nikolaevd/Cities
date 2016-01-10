8f34b173cbf852546e2ebd52633f83317558e593
Denis Nikolaev <idenisnikolaev@itsph.ru>
2016-01-10 12:34:01 +0300

    current snapshot

--- src/java/com/example/conroller/set/SecondSetHandler.java
+++ src/java/com/example/conroller/set/SecondSetHandler.java
@@ -1,7 +1,9 @@
 
 package com.example.conroller.set;
 
+import com.example.model.set.SecondSet;
 import java.io.IOException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +15,14 @@ public class SecondSetHandler extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        
+        int secondSet = SecondSet.setData(request.getParameter("id"), request.getParameter("nationality"), request.getParameter("incipience"));
+        
+        request.setAttribute("secondSet", secondSet);
+        RequestDispatcher view = request.getRequestDispatcher("result.jsp");
+        view.forward(request, response);
         
     }
 
