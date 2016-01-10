297b7b53fda9300f8512c1b5b56e3320b248df36
Denis Nikolaev <idenisnikolaev@itsph.ru>
2016-01-10 13:02:46 +0300

    current snapshot

--- src/java/com/example/conroller/set/ThirdSetHandler.java
+++ src/java/com/example/conroller/set/ThirdSetHandler.java
@@ -1,7 +1,9 @@
 
 package com.example.conroller.set;
 
+import com.example.model.set.ThirdSet;
 import java.io.IOException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +15,14 @@ public class ThirdSetHandler extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        
+        int thirdSet = ThirdSet.setData(request.getParameter("nationality_id"), request.getParameter("city_id"), request.getParameter("population"));
+        
+        request.setAttribute("thirdSet", thirdSet);
+        RequestDispatcher view = request.getRequestDispatcher("result.jsp");
+        view.forward(request, response);
         
     }
 
--- src/java/com/example/model/set/ThirdSet.java
+++ src/java/com/example/model/set/ThirdSet.java
@@ -1,6 +1,44 @@
 
 package com.example.model.set;
 
+import com.example.model.CitiesDB;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class ThirdSet {
+    
+     public static int setData(String nationality_id, String city_id, String population){
+        
+        Connection connection = null;
+        Statement statement = null;
+        
+        try{
+            connection = CitiesDB.getConnection();
+            
+            String sql = "INSERT INTO population (nationality_id, city_id, population)\n" +
+                "VALUES (" + Integer.parseInt(nationality_id)+ ", " + Integer.parseInt(city_id) + ", " + Integer.parseInt(population) + ")";
+            
+            statement = connection.createStatement();
+            statement.execute(sql);
+            
+            statement.close();
+            connection.close();
+            
+            return 1;
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        
+        return 0;
+        
+    }
     
 }
